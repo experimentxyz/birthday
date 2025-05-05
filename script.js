ef60d0a7d1c655de240e3198ab169e46cc6977cc
YuvrajModhavadiya <yuvrajmodhavadiya09@gmail.com>
2025-05-05 15:19:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper', {
+      loop: true,
+      speed: 1000, // 1000ms = 1 second for smooth transition
+      autoplay: {
+        delay: 1000, // 3 seconds between slides
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      slidesPerView: 1,
+      spaceBetween: 20,
+      breakpoints: {
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 2,
+        },
+      },
+    });
+  });
+  
